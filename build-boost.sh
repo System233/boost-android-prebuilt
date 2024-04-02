87c444f93299de5c7b934dff1638d1ebe72cf1ef
System233 <cdkeylzh@outlook.com>
2024-04-02 17:47:12 +0800

    test: SDK 19,20

--- build-boost.sh
+++ build-boost.sh
@@ -27,11 +27,7 @@ cmake .. -DCMAKE_TOOLCHAIN_FILE=$NDK_HOME/build/cmake/android.toolchain.cmake \
         -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS \
         -DBOOST_LOCALE_ENABLE_POSIX=off \
         -DBOOST_LOCALE_ENABLE_ICONV=off \
-        -DBOOST_LOCALE_ENABLE_STD=on \
-        -DBOOST_LOCALE_ENABLE_ICU=on \
-        -DICU_INCLUDE_DIR=$BUILD_DIST_DIR/include \
-        -DICU_UC_LIBRARY_RELEASE=$BUILD_DIST_DIR/lib/libicuuc.so \
-        -DICU_I18N_LIBRARY_RELEASE=$BUILD_DIST_DIR/lib/libicui18n.so \
-        -DICU_DATA_LIBRARY_RELEASE=$BUILD_DIST_DIR/lib/libicudata.so
+        -DBOOST_LOCALE_ENABLE_STD=off \
+        -DBOOST_LOCALE_ENABLE_ICU=on
 cmake --build . --config Release
 cmake --install . --prefix=$BUILD_DIST_DIR
