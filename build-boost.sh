c8e137f18b66a861a691af4f2979e315582a0df2
System233 <cdkeylzh@outlook.com>
2024-04-02 19:43:32 +0800

    test: FindICU

--- build-boost.sh
+++ build-boost.sh
@@ -17,8 +17,10 @@ cd build
 
 if [ "$BUILD_TYPE" == "static" ];then
         BUILD_SHARED_LIBS=off
+        LIBRARY_SUFFIX=a
 else
         BUILD_SHARED_LIBS=on
+        LIBRARY_SUFFIX=so
 fi
 
 cmake .. -DCMAKE_TOOLCHAIN_FILE=$NDK_HOME/build/cmake/android.toolchain.cmake \
@@ -29,9 +31,9 @@ cmake .. -DCMAKE_TOOLCHAIN_FILE=$NDK_HOME/build/cmake/android.toolchain.cmake \
         -DBOOST_LOCALE_ENABLE_ICONV=off \
         -DBOOST_LOCALE_ENABLE_STD=off \
         -DBOOST_LOCALE_ENABLE_ICU=on \
-        -DICU_ROOT=$BUILD_DIST_DIR \
-        -DICU_DEBUG=on \
-        -DCMAKE_INCLUDE_PATH=$BUILD_DIST_DIR/include \
-        -DCMAKE_LIBRARY_PATH=$BUILD_DIST_DIR/lib
+        -DICU_INCLUDE_DIR=$BUILD_DIST_DIR/include \
+        -DICU_UC_LIBRARY_RELEASE=$BUILD_DIST_DIR/lib/libicuuc.$LIBRARY_SUFFIX \
+        -DICU_I18N_LIBRARY_RELEASE=$BUILD_DIST_DIR/lib/libicui18n.$LIBRARY_SUFFIX \
+        -DICU_DATA_LIBRARY_RELEASE=$BUILD_DIST_DIR/lib/libicudata.$LIBRARY_SUFFIX
 cmake --build . --config Release
 cmake --install . --prefix=$BUILD_DIST_DIR
